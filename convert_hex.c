19ff486955fefc74e1b775ef43a00ae098827452
Skyprincess <iyaseamaka@gmail.com>
2022-11-24 11:54:17 -0800

    convert_hex.cc

--- convert_hex.c
+++ convert_hex.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 unsigned int convert_x(va_list args, buffer_t *output,
-				unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_X(va_list args, buffer_t *output,
-				unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 
 /**
  * convert_x - Converts an unsigned int argument to hex using abcdef
@@ -18,7 +18,7 @@ unsigned int convert_X(va_list args, buffer_t *output,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_x(va_list args, buffer_t *output,
-			unsigned char flags, int wid, int prec, unsigned char len)
+		unsigned char flags, int wid, int prec, unsigned char len)
 {
 	unsigned long int num;
 	unsigned int ret = 0;
@@ -30,8 +30,10 @@ unsigned int convert_x(va_list args, buffer_t *output,
 		num = va_arg(args, unsigned int);
 	if (len == SHORT)
 		num = (unsigned short)num;
+
 	if (HASH_FLAG == 1 && num != 0)
 		ret += _memcpy(output, lead, 2);
+
 	if (!(num == 0 && prec == 0))
 		ret += convert_ubase(output, num, "0123456789abcdef",
 				flags, wid, prec);
@@ -64,11 +66,15 @@ unsigned int convert_X(va_list args, buffer_t *output,
 		num = va_arg(args, unsigned int);
 	if (len == SHORT)
 		num = (unsigned short)num;
+
 	if (HASH_FLAG == 1 && num != 0)
 		ret += _memcpy(output, lead, 2);
+
 	if (!(num == 0 && prec == 0))
 		ret += convert_ubase(output, num, "0123456789ABCDEF",
 				flags, wid, prec);
+
 	ret += print_neg_width(output, ret, flags, wid);
+
 	return (ret);
 }
