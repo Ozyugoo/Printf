af7a4ac5298923cecd32b0ca88b6cd0b859a1968
Skyprincess <iyaseamaka@gmail.com>
2022-11-24 00:41:34 -0800

    convert_hex.c

--- convert_hex.c
+++ convert_hex.c
@@ -20,28 +20,23 @@ unsigned int convert_X(va_list args, buffer_t *output,
 unsigned int convert_x(va_list args, buffer_t *output,
 			unsigned char flags, int wid, int prec, unsigned char len)
 {
-		unsigned long int num;
-		unsigned int ret = 0;
-		char *lead = "0x";
+	unsigned long int num;
+	unsigned int ret = 0;
+	char *lead = "0x";
 
-		if (len == LONG)
-			num = va_arg(args, unsigned long int);
-
-		else
-			num = va_arg(args, unsigned int);
-
-		if (len == SHORT)
-			num = (unsigned short)num;
-
-		if (HASH_FLAG == 1 && num != 0)
-			ret += _memcpy(output, lead, 2);
-		if (!(num == 0 && prec == 0))
+	if (len == LONG)
+		num = va_arg(args, unsigned long int);
+	else
+		num = va_arg(args, unsigned int);
+	if (len == SHORT)
+		num = (unsigned short)num;
+	if (HASH_FLAG == 1 && num != 0)
+		ret += _memcpy(output, lead, 2);
+	if (!(num == 0 && prec == 0))
 		ret += convert_ubase(output, num, "0123456789abcdef",
-					flags, wid, prec);
-
-		ret += print_neg_width(output, ret, flags, wid);
-
-			return (ret);
+				flags, wid, prec);
+	ret += print_neg_width(output, ret, flags, wid);
+	return (ret);
 }
 
 /**
@@ -57,26 +52,23 @@ unsigned int convert_x(va_list args, buffer_t *output,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_X(va_list args, buffer_t *output,
-			unsigned char flags, int wid, int prec, unsigned char len)
+		unsigned char flags, int wid, int prec, unsigned char len)
 {
-		unsigned long int num;
-		unsigned int ret = 0;
-		char *lead = "0X";
+	unsigned long int num;
+	unsigned int ret = 0;
+	char *lead = "0X";
 
-		if (len == LONG)
-			num = va_arg(args, unsigned long);
-		else
-			num = va_arg(args, unsigned int);
-
-		if (len == SHORT)
-			num = (unsigned short)num;
-
-		if (HASH_FLAG == 1 && num != 0)
-			ret += _memcpy(output, lead, 2);
-
-		if (!(num == 0 && prec == 0))
+	if (len == LONG)
+		num = va_arg(args, unsigned long);
+	else
+		num = va_arg(args, unsigned int);
+	if (len == SHORT)
+		num = (unsigned short)num;
+	if (HASH_FLAG == 1 && num != 0)
+		ret += _memcpy(output, lead, 2);
+	if (!(num == 0 && prec == 0))
 		ret += convert_ubase(output, num, "0123456789ABCDEF",
-					flags, wid, prec);
-		ret += print_neg_width(output, ret, flags, wid);
-					return (ret);
+				flags, wid, prec);
+	ret += print_neg_width(output, ret, flags, wid);
+	return (ret);
 }
